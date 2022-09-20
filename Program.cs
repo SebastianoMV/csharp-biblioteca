54a2d8a716088aea4cd92b0aa7feffbf0bdb5894
SebastianoMV <sebastianovisco@gmail.com>
2022-09-20 16:12:55 +0200

    Modifica Prodotto in Documento e prove di prime ricerche dell'utente

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("LIBRERIA BELLA.");
+using System.IO;
+
 
 Console.WriteLine("  ");
+List<Utente> utentes = new List<Utente>();
+utentes.Add(new Utente("Ugo", "De Ughi", "ugo@gmail.com", "qwerty1", "345223443"));
 
 
 
@@ -14,3 +17,39 @@ List <Libro> libros = new List<Libro>();
 libros.Add(new Libro(9780261103665, "Il Silmarillion", 1977, "Fantasy", false, 5, "Tolkien", 1000));
 libros.Add(new Libro(9788877827029, "Harry Potter", 1997, "Fantasy", false, 3, "J. K. Rowling", 1000));
 libros.Add(new Libro(9780151660346, "1984", 1949, "Distopico", false, 3, "George Orwell", 1000));
+
+
+foreach (Dvd dvd in dvds)
+{
+    Console.WriteLine(dvd.GetCodice);
+}
+
+string ricerca;
+Console.WriteLine("Vuoi cercare un libro o un dvd?");
+ricerca = Console.ReadLine();
+if (ricerca == "dvd")
+{
+    Console.WriteLine("Inserisci un Titolo");
+    string titoloRicerca = Console.ReadLine();
+    foreach (Dvd dvd in dvds)
+    {
+       
+        if(dvd.GetTitolo == titoloRicerca)
+        {
+            Console.WriteLine(dvd.GetCodice);
+        }
+    }
+    
+
+
+
+
+}
+else if(ricerca == "libri")
+{
+
+}
+else
+{
+    Console.WriteLine("Campo non valido");
+}
