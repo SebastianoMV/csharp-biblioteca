5195e52180c235bd296461ffbee64c381575e77b
SebastianoMV <sebastianovisco@gmail.com>
2022-09-20 15:18:07 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("LIBRERIA BELLA.");
+
+Console.WriteLine("  ");
+
+
+
+List <Dvd> dvds = new List<Dvd>();
+dvds.Add(new Dvd(12331, "Signore degli Anelli", 2000, "Fantasy", false, 5, "Peter Jackson", 120));
+dvds.Add(new Dvd(1444, "Le Follie Dell'imperatore", 2000, "Commedia", false, 8, "Mark Dindal", 78));
+dvds.Add(new Dvd(211111, "Il cavaliere oscuro", 2008, "Azione", false, 20, "Christopher Nolan", 152));
+
+List <Libro> libros = new List<Libro>();
+libros.Add(new Libro(9780261103665, "Il Silmarillion", 1977, "Fantasy", false, 5, "Tolkien", 1000));
+libros.Add(new Libro(9788877827029, "Harry Potter", 1997, "Fantasy", false, 3, "J. K. Rowling", 1000));
+libros.Add(new Libro(9780151660346, "1984", 1949, "Distopico", false, 3, "George Orwell", 1000));
---
+++ Utente.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class Utente
+{
+    string nome;
+    string cognome;
+    string email;
+    string password;
+    string telefono;
+
+    public Utente(string nome, string cognome, string email, string password, string telefono)
+    {
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.password = password;
+        this.telefono = telefono;
+    }
+}
